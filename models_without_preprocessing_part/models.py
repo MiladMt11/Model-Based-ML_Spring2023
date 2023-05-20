926c0633817e39c29f8ec205b6d0a01e9f8022bd
MNIS (Mihaela-Elena Nistor) <MNIS@novozymes.com>
2023-05-20 18:07:40 +0200

    move model definitions to model file and refactor code to load the models from the models module

---
+++ models_without_preprocessing_part/models.py
@@ -0,0 +1,100 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+from sklearn import linear_model
+import torch
+from scipy.special import softmax 
+from sklearn.preprocessing import StandardScaler
+
+import pyro
+import pyro.distributions as dist
+from pyro.contrib.autoguide import AutoDiagonalNormal, AutoMultivariateNormal
+from pyro.infer import MCMC, NUTS, HMC, SVI, Trace_ELBO
+from pyro.optim import Adam, ClippedAdam
+from sklearn.model_selection import train_test_split
+
+
+def model(X, n_cat, obs=None):
+    input_dim = X.shape[1]
+    alpha = pyro.sample("alpha", dist.Normal(torch.zeros(1, n_cat), 
+                                             5.*torch.ones(1, n_cat)).to_event())  # Prior for the bias/intercept
+    beta  = pyro.sample("beta", dist.Normal(torch.zeros(input_dim, n_cat), 
+                                            5.*torch.ones(input_dim, n_cat)).to_event()) # Priors for the regression coeffcients
+    
+    with pyro.plate("data"):
+        y = pyro.sample("y", dist.Categorical(logits=alpha + X.matmul(beta)), obs=obs)
+        
+    return y
+
+def model__prior_mu_studentT(X, n_cat, degF, tau, obs=None):
+    
+    input_dim = X.shape[1]
+    
+    
+    mu_beta = pyro.sample("mu_beta", dist.StudentT(df=torch.ones(n_cat)*degF, 
+                                                   loc=torch.zeros(n_cat), 
+                                                   scale=torch.ones(n_cat)).to_event()) # Prior for the bias mean      
+    sigma_beta  = pyro.sample("sigma_beta",  dist.HalfCauchy(tau*torch.ones(n_cat)).to_event()) # Prior for the bias standard deviation
+    
+    beta  = pyro.sample("beta", dist.Normal(mu_beta*torch.ones(n_cat), 
+                                            sigma_beta*torch.ones(input_dim, n_cat)).to_event()) # Priors for the regression coefficents
+
+    alpha = pyro.sample("alpha", dist.Normal(torch.zeros(1, n_cat), 
+                                             5.*torch.ones(1, n_cat)).to_event())  # Prior for the bias/intercept
+    
+    
+    with pyro.plate("data"):
+        y = pyro.sample("y", dist.Categorical(logits=alpha + X.matmul(beta)), obs=obs)
+        
+    return y
+
+
+def model_prior_mu_normal(X, n_cat,lambda_, tau, obs=None):
+    
+    input_dim = X.shape[1]
+    
+    
+    mu_beta = pyro.sample("mu_beta", dist.Normal(torch.zeros(n_cat), 
+                                            lambda_*torch.ones(n_cat)).to_event()) # Prior for the bias mean  
+    
+    sigma_beta  = pyro.sample("sigma_beta",  dist.HalfCauchy(tau*torch.ones(n_cat)).to_event()) # Prior for the bias standard deviation
+    
+    beta  = pyro.sample("beta", dist.Normal(mu_beta*torch.ones(n_cat), 
+                                            sigma_beta*torch.ones(input_dim, n_cat)).to_event()) # Priors for the regression coefficents
+
+    alpha = pyro.sample("alpha", dist.Normal(torch.zeros(1, n_cat), 
+                                             5.*torch.ones(1, n_cat)).to_event())  # Prior for the bias/intercept
+    
+    
+    with pyro.plate("data"):
+        y = pyro.sample("y", dist.Categorical(logits=alpha + X.matmul(beta)), obs=obs)
+        
+    return y
+
+def model_ARD(X, n_cat, degF, tau, obs=None):
+    
+    input_dim = X.shape[1]
+    
+    mu_beta = pyro.sample("mu_beta", dist.StudentT(df=torch.ones(n_cat)*degF, 
+                                                    loc=torch.zeros(n_cat), 
+                                                    scale=torch.ones(n_cat)).to_event()) # Prior for the bias mean      
+    
+    with pyro.plate("ARD", input_dim):
+
+        sigma_beta  = pyro.sample("sigma_beta",  dist.HalfCauchy(tau*torch.ones(n_cat)).to_event()) # Prior for the bias standard deviation
+        
+    beta  = pyro.sample("beta", dist.Normal(mu_beta*torch.ones(n_cat), 
+                                            sigma_beta).to_event()) # Priors for the regression coefficents
+
+    alpha = pyro.sample("alpha", dist.Normal(torch.zeros(1, n_cat), 
+                                             5.*torch.ones(1, n_cat)).to_event())  # Prior for the bias/intercept
+    
+    
+    with pyro.plate("data"):
+        y = pyro.sample("y", dist.Categorical(logits=alpha + X.matmul(beta)), obs=obs)
+        
+    return y
+
+    
